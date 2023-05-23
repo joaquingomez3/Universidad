0267fa1f523924369a1c0fd698106bdd0749b631
matheredia5 <104038470+matheredia5@users.noreply.github.com>
2023-05-23 10:47:01 -0300

    materia data y parte de inscripcion

--- src/Control/InscripcionData.java
+++ src/Control/InscripcionData.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Control;
 
-/**
- *
- * @author pc
- */
+import java.awt.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import modelo.Inscripcion;
+
+
+
 public class InscripcionData {
     
+    public List<Inscripcion> obtenerInscripciones(){
+        List<Inscripcion> cursadas = new ArrayList<>();
+          
+        try {
+            String sql = "SELECT * FROM inscripcion;";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            Inscripcion insc;
+            
+            while(rs.next()){
+                insc = new Inscripcion();
+                insc.setIdInscripcion(rs.getInt("idInscripto"));
+                
+                Alumno a = aluData.buscarAlumno(rs.getInt("idAlumno"));
+                insc.setIdAlumno(a.getIdAlumno());
+                
+                Materia m = matData.buscarMateria(rs.getInt("idMateria"));
+                insc.setIdMateria(m.getIdMateria());
+                insc.setNota(rs.getDouble("nota"));
+
+                cursadas.add(insc);
+            }      
+            ps.close();
+        }catch (SQLException ex) {
+            JOptionPane.showInternalMessageDialog(null, "Error Inscripcion "+ex.getMessage());
+        }
+        return cursadas;
+    }
 }
--- src/Control/MateriaData.java
+++ src/Control/MateriaData.java
@@ -1,14 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Control;
 
-/**
- *
- * @author pc
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Materia;
+
 public class MateriaData {
+    private Connection con;
     
-}
+    public MateriaData(){
+        con= Conexion.getConexion();
+    }
+    
+     public void cargarMateria(Materia materia){
+        try {
+            String sql= "INSERT INTO materia (nombre, año, estado) values(?,?,?)";
+            
+            PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1,materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if(rs.next()){
+                materia.setIdMateria(rs.getInt(1));
+            }else
+                System.out.println("El alumno no se pudo guardar");
+        
+             ps.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AlumnoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+     public void actualizarMateria(Materia materia){
+        try {
+            String sql= "UPDATE materia SET id_materia=?, nombre=?, año=?, estado=? WHERE 1";
+            
+            PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, materia.getIdMateria());
+            ps.setString(2, materia.getNombre());
+            ps.setInt(3, materia.getAnio());
+            ps.setBoolean(4, materia.isEstado());          
+            ps.executeUpdate();
+            
+            ps.close();
+            
+    } catch (SQLException ex) {
+            Logger.getLogger(MateriaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+    }
+     
+     public Materia buscarMateria(int idMateria){
+        Materia mat= null;
+       String query = "SELECT id_materia, nombre, año, estado FROM materia WHERE id_materia=?";
+        PreparedStatement ps;
+        try{
+            ps=con.prepareStatement (query);
+            ps.setInt(1, idMateria);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs.next()){
+                mat=new Materia();                
+                mat.setIdMateria( idMateria );               
+                mat.setNombre(rs.getString("nombre"));
+                mat.setAnio(rs.getInt("anio"));               
+                mat.setEstado(rs.getBoolean("Activo"));
+                
+                }else
+                    System.out.println("Materia inexistente");
+                    
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaData.class.getName()).log(Level.SEVERE, null, ex.getMessage());
+        }
+        return null;
+    }  
+}    
